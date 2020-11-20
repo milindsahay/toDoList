c0ccdc85cec91e058902d52037937709ae2a6ea5
milindsahay <milind.sahay123@gmail.com>
2020-11-20 22:03:40 +0530

    add npm start and work api endpoint

--- app.js
+++ app.js
@@ -2,23 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date');
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 let toDos = [];
+let workToDos = [];
 app.use(express.static(__dirname + "/views"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
-    res.render('index', {date: date(), toDos: toDos});
+    res.render('index', {date: date(), toDos: toDos, type: "To Do"});
 })
-
+app.get('/work', (req, res) => {
+    res.render('index', {date: date(), toDos: workToDos, type: "Work"})
+})
 app.post('/', (req, res) => {
     let newTask = req.body.newTask;
-    if (newTask.length){
-        toDos.push(req.body.newTask);
+    if (newTask.length) {
+        toDos.push(newTask);
     }
     res.redirect('/');
+})
+app.post('/work', (req, res) => {
+    let newTask = req.body.newTask;
+    if (newTask.length) {
+        workToDos.push(newTask);
+    }
+    res.redirect('/work');
 })
 app.listen(PORT, () => {
     console.log("Server Started on port " + PORT);
