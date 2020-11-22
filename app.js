33b2b6c43e3a5b5728df5fba8d0fbb8ac550d93d
milindsahay <milind.sahay123@gmail.com>
2020-11-22 20:04:47 +0530

    mongoDB local create integration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date');
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +10,22 @@ let workToDos = [];
 app.use(express.static(__dirname + "/views"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost:27017/toDoListDB', {useNewUrlParser: true});
+const itemSchema = {
+    item: String
+}
+const Item = mongoose.model('Item', itemSchema);
+const item1 = new Item({item: "Welcome to your To do list"});
+const item2 = new Item({item: "Hit + to add new item"});
+const item3 = new Item({item: "<-- hit this to delete this item"});
+const defaultItems = [item1, item2, item3];
+Item.insertMany(defaultItems, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("DB updated successfully");
+    }
+})
 app.get("/", (req, res) => {
     res.render('index', {date: date(), toDos: toDos, type: "To Do"});
 })
